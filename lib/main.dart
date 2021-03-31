41c2fa680e259b37d4ea08f2588be5d7ecc46167
Prabhu Nath Tiwary <prabhunt@micelio.local>
2021-03-31 13:47:27 +0530

    created daily page

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:budget_tracker/pages/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: RootApp(),
+));
---
+++ lib/pages/root_app.dart
@@ -0,0 +1,92 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:budget_tracker/pages/daily_page.dart';
+import 'package:budget_tracker/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class RootApp extends StatefulWidget {
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+      bottomNavigationBar: getFooter(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            setTabs(4);
+          });
+        },
+        child: Icon(
+          Icons.add,
+          size: 25,
+        ),
+        backgroundColor: primary,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: pageIndex,
+      children: [
+        DailyPage(),
+        Center(
+          child: Text(
+              'Stats Page'
+          ),
+        ),
+        Center(
+          child: Text(
+              'Budget Page'
+          ),
+        ),
+        Center(
+          child: Text(
+              'Profile Page'
+          ),
+        ),
+        Center(
+          child: Text(
+              'Create budget  Page'
+          ),
+        ),
+      ],
+    );
+
+  }
+
+  Widget getFooter() {
+    List<IconData> iconItems = [
+      Ionicons.md_calendar,
+      Ionicons.md_stats,
+      Ionicons.md_wallet,
+      Ionicons.ios_person,
+    ];
+    return AnimatedBottomNavigationBar(
+        icons: iconItems,
+        activeColor: primary,
+        splashColor: secondary,
+        inactiveColor: black.withOpacity(0.5),
+        activeIndex: pageIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+        leftCornerRadius: 10,
+        iconSize: 25,
+        rightCornerRadius: 10,
+        onTap: (index){
+          setTabs(index);
+        });
+        }
+    setTabs(index){
+    setState(() {
+      pageIndex = index;
+    });
+    }
+      }
